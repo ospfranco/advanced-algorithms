947f49d9f46af3951f3d7879e9fff639eefcb208
Oscar Franco <ospfranco@gmail.com>
2013-11-26 12:40:18 -0400

    uva 10065 useless tile packers WORKING with graham scan

--- src/paraExamen3/UselessTilePackers_uva10065.java
+++ src/paraExamen3/UselessTilePackers_uva10065.java
@@ -9,38 +9,25 @@ import java.util.Scanner;
 
 public class UselessTilePackers_uva10065 {
 	static Punto p0;
-	static int[] xPoints, yPoints;
-	static int xPoints2[];
-    static int yPoints2[];
-    static int num, pNum;
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
 		int T = 1;
 		while(n != 0){
-			xPoints = new int[n];
-			yPoints = new int[n];
-			num = 0;
-			pNum = n;
+			Punto[] points = new Punto[n];
 			
 			for (int i = 0; i < n; i++) {
-				xPoints[i] = scan.nextInt();
-				yPoints[i] = scan.nextInt();
+				points[i] = new Punto(scan.nextInt(), scan.nextInt());
 			}
 			
-			xPoints2 = new int[n];
-			yPoints2 = new int[n];
-		
-			double areaPolygon = polygonArea(xPoints, yPoints, n);
 			
-			quickconvexhull();
+			double areaPolygon = polygonArea(points, n);
 			
-			double areaHull = polygonArea(xPoints2, yPoints2, xPoints2.length);
+			Punto[] hull = convexHull(points, n);
+			double areaHull = polygonArea(hull, hull.length);
 			
-			double wastedSpace = ((areaHull-areaPolygon)/areaHull)*100;
-			
-			System.out.println("area poligono: "+areaPolygon+" Area Hull: "+areaHull+" Area desperdiciada: "+((double)areaHull-(double)areaPolygon)+" division: "+wastedSpace);
-			
+			double wastedSpace = (((double)areaHull-(double)areaPolygon)/(double)areaHull)*100;
+			//System.out.println("area poligono: "+areaPolygon+" Area Hull: "+areaHull+" Area desperdiciada: "+((double)areaHull-(double)areaPolygon)+" division: "+wastedSpace);
 			String res = "Tile #"+T+"\n";
 			res += String.format("Wasted Space = %.2f", wastedSpace) + " %\n";
 			System.out.println(res);
@@ -51,123 +38,84 @@ public class UselessTilePackers_uva10065 {
 
 	}
 
-	private static double polygonArea(int[] xs, int[] ys, int n) {
+	private static double polygonArea(Punto[] p, int n) {
 		double area = 0;
 		int j = n - 1;
 		for (int i = 0; i < n; i++) {
-			area += (xs[j] +xs[i]) * (ys[j]-ys[i]);
+			area += (p[j].x +p[i].x) * (p[j].y-p[i].y);
 			j = i;
 		}
 		return Math.abs(area / 2);
 	}
 	
-
-    // check whether point p is right of line ab
-    public static int right(int a, int b, int p)
-    {
-	return (xPoints[a] - xPoints[b])*(yPoints[p] - yPoints[b]) - (xPoints[p] - xPoints[b])*(yPoints[a] - yPoints[b]);
-    }
-
-    // square distance point p to line ab
-    public static float distance(int a, int b, int p)
-    {
-	float x, y, u;
-	u = (((float)xPoints[p] - (float)xPoints[a])*((float)xPoints[b] - (float)xPoints[a]) + ((float)yPoints[p] - (float)yPoints[a])*((float)yPoints[b] - (float)yPoints[a])) 
-	    / (((float)xPoints[b] - (float)xPoints[a])*((float)xPoints[b] - (float)xPoints[a]) + ((float)yPoints[b] - (float)yPoints[a])*((float)yPoints[b] - (float)yPoints[a]));
-	x = (float)xPoints[a] + u * ((float)xPoints[b] - (float)xPoints[a]);
-	y = (float)yPoints[a] + u * ((float)yPoints[b] - (float)yPoints[a]);
-	return ((x - (float)xPoints[p])*(x - (float)xPoints[p]) + (y - (float)yPoints[p])*(y - (float)yPoints[p]));
-    }
-
-    public static int farthestpoint(int a, int b, ArrayList<Integer>al)
-    {
-	float maxD, dis;
-	int maxP, p;
-	maxD = -1;
-	maxP = -1;
-	for ( int i = 0; i < al.size(); i++ ) {
-	    p = al.get(i);
-	    if ( (p == a) || (p == b) )
-		continue;
-	    dis = distance(a, b, p);
-	    if ( dis > maxD ) {
-		maxD = dis;
-		maxP = p;
-	    }
+	private static Punto[] convexHull(Punto[] puntos, int n) {
+		int ymin = puntos[0].y, min = 0;
+		for (int i = 1; i < n; i++) {
+			
+			int y = puntos[i].y;
+			
+			if(y < ymin || (ymin == y && puntos[i].x < puntos[min].x)){
+				ymin = puntos[i].y; min = i;
+			}
+		}
+		
+		swap(puntos, 0, min);
+		
+		p0 = puntos[0]; 
+		Comp c = new Comp();
+		Arrays.sort(puntos, c);
+		
+		//System.out.println("punto base "+p0.x+" "+p0.y);
+		//puntos = Arrays.copyOf(puntos, n+1);
+		//puntos[n] = new Punto(p0.x, p0.y);
+		
+		Deque<Punto> stack = new ArrayDeque<Punto>();
+		stack.push(puntos[0]);
+		stack.push(puntos[1]);
+		stack.push(puntos[2]);
+		
+		for (int i = 3; i < n; i++) {
+			while(orientation(nextToTop(stack), stack.peek(), puntos[i])!=2){
+				stack.pop();
+			}
+			stack.push(puntos[i]);
+		}
+		
+		Punto[] res = new Punto[stack.size()];
+		for (int i = 0; i < res.length; i++) {
+			res[i] = stack.pop();
+		//	System.out.println("punto "+i+" "+res[i].x+" "+res[i].y);
+		}
+		/*System.out.println("puntos del hull:");
+		while(!stack.isEmpty()){
+			System.out.println(stack.peek().x +" "+ stack.peek().y);
+			stack.pop();
+		}
+		System.out.println("----------");*/
+		return res;
 	}
-	return maxP;
-    }
-
-    public static void quickhull(int a, int b, ArrayList<Integer>al)
-    {
-	//System.out.println("a:"+a+",b:"+b+" size: "+al.size());
-	if ( al.size() == 0 )
-	    return;
-
-	int c, p;
-
-	c = farthestpoint(a, b, al);
-
-	ArrayList<Integer> al1 = new ArrayList<Integer>();
-	ArrayList<Integer> al2 = new ArrayList<Integer>();
-
-	for ( int i=0; i<al.size(); i++ ) {
-	    p = al.get(i);
-	    if ( (p == a) || (p == b) )
-		continue;
-	    if ( right(a,c,p) > 0 )
-		al1.add(p);
-	    else if ( right(c,b,p) > 0 )
-		al2.add(p);
-	}
-
-	quickhull(a, c, al1);
-	xPoints2[num] = xPoints[c];
-	yPoints2[num] = yPoints[c];
-	num++;
-	quickhull(c, b, al2);
-    }
-
-    public static void quickconvexhull() {
-
-	// find two points: right (bottom) and left (top)
-	int r, l;
-	r = l = 0;
-	for ( int i = 1; i < pNum; i++ ) {
-	    if ( ( xPoints[r] > xPoints[i] ) || ( xPoints[r] == xPoints[i] && yPoints[r] > yPoints[i] ))
-	    	r = i;
-	    if ( ( xPoints[l] < xPoints[i] ) || ( xPoints[l] == xPoints[i] && yPoints[l] < yPoints[i] ))
-	    	l = i;
-	}
-
-	//System.out.println("l: "+l+", r: "+r);
-
-	ArrayList<Integer> al1 = new ArrayList<Integer>();
-	ArrayList<Integer> al2 = new ArrayList<Integer>();
-
-	int upper;
-	for ( int i = 0; i < pNum; i++ ) {
-	    if ( (i == l) || (i == r) )
-		continue;
-	    upper = right(r,l,i);
-	    if ( upper > 0 )
-		al1.add(i);
-	    else if ( upper < 0 )
-		al2.add(i);
-	}
-
-	xPoints2[num] = xPoints[r];
-	yPoints2[num] = yPoints[r];
-	num++;
-	quickhull(r, l, al1);
-	xPoints2[num] = xPoints[l];
-	yPoints2[num] = yPoints[l];
-	num++;
-	quickhull(l, r, al2);
-    }
-
 	
+	private static int orientation(Punto p, Punto q, Punto r) {
+		 int val = (q.y - p.y) * (r.x - q.x) -
+	              (q.x - p.x) * (r.y - q.y);
+	 
+	    if (val == 0) return 0;  // colinear
+	    return (val > 0)? 1: 2;
+	}
 	
+	private static Punto nextToTop(Deque<Punto> s){
+		Punto p = s.poll();
+		Punto res = s.peek();
+		s.push(p);
+		return res;
+	}
+	
+	private static void swap(Punto[] points, int p1, int p2){
+		Punto aux = points[p1];
+		points[p1] = points[p2];
+		points[p2] = aux;
+	}
+
 	static class Punto{
 		public int x,y;
 		public Punto(int x, int y){
@@ -176,5 +124,28 @@ public class UselessTilePackers_uva10065 {
 		}
 	}
 	
+	static class Comp implements Comparator<Punto>{
+
+		@Override
+		public int compare(Punto p1, Punto p2) {
+			int o = orientation(p0, p1, p2);
+			if(o == 0)
+				return (dist(p0, p2) >= dist(p0, p1))? -1 : 1;
+			return (o == 2)? -1: 1;
+		}
+
+		private int dist(Punto p1, Punto p2) {
+			 return (p1.x - p2.x)*(p1.x - p2.x) + (p1.y - p2.y)*(p1.y - p2.y);
+		}
+
+		private int orientation(Punto p, Punto q, Punto r) {
+			 int val = (q.y - p.y) * (r.x - q.x) -
+		              (q.x - p.x) * (r.y - q.y);
+		 
+		    if (val == 0) return 0;  // colinear
+		    return (val > 0)? 1: 2;
+		}
+		
+	}
 
 }
